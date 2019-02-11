11472741d01cc16fce0ac4d7b7ba76340049449d
Alefe Souza <contact@alefesouza.com>
2019-02-11 00:30:43 -0200

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+function subtrai(n1, n2) {
+    return n1 - n2;
+}
+
+module.exports = {
+    soma,
+    subtrai
+};
---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const file = require('../src/index');
+
+describe('Testes da função soma', () => {
+
+    it('deve resultar em 3 ao executar a função soma', () => {
+        expect(file.soma(1, 2)).toBe(3);
+    });
+
+});
+
+describe('Testes da função subtrai', () => {
+
+    it('deve resultar em 2 ao executar a função subtrai', () => {
+        expect(file.subtrai(4, 2)).toBe(2);
+    });
+
+});
